d0364970b13fca1b2c13c93ee48d320f5e24febe
Ronin <yato050303@rambler.ru>
2023-03-04 23:38:24 +1000

    design fix

--- app/src/main/java/com/example/pokemonbase/ui/HomeScreenViewModel.kt
+++ app/src/main/java/com/example/pokemonbase/ui/HomeScreenViewModel.kt
@@ -1,10 +1,15 @@
 package com.example.pokemonbase.ui
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
+import androidx.palette.graphics.Palette
 import com.example.pokemonbase.models.PokemonListEntity
 import com.example.pokemonbase.network.PokemonServicePagingSource
 import com.example.pokemonbase.repositories.PokemonServiceRepository
@@ -14,6 +19,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -26,11 +32,21 @@ class HomeScreenViewModel @Inject constructor(
     val pokemonPager = Pager(
       PagingConfig(
           pageSize = Constants.PAGE_SIZE,
-          initialLoadSize = Constants.PAGE_SIZE*3,
-          enablePlaceholders = true
+          initialLoadSize = Constants.PAGE_SIZE*3
       )
     ){
         pokemonServicePagingSource
   }.flow.cachedIn(viewModelScope)
+
+    fun calcDominantColor(drawable:Drawable, onFinish:(Color)->Unit){
+        viewModelScope.launch(Dispatchers.IO) {
+            val bitmap = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888,true)
+            Palette.from(bitmap).generate { palette ->
+                palette?.dominantSwatch?.rgb?.let { colorValue ->
+                    onFinish(Color(colorValue))
+                }
+            }
+        }
+    }
 
 }
